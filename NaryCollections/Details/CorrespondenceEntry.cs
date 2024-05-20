9bee41bfc2e2f01e8dec63cb2a9fd8413e7618de
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2024-05-20 21:44:51 +0200

    Add debug display to entry

--- NaryCollections/Details/CorrespondenceEntry.cs
+++ NaryCollections/Details/CorrespondenceEntry.cs
@@ -1,11 +1,29 @@
+using System.Diagnostics;
+
 namespace NaryCollections.Details;
 
+[DebuggerDisplay("{this.Debug()}")]
 public struct CorrespondenceEntry
 {
     public static readonly int NoNextCorrespondence = -1;
     
-    public int BackIndex; // index in the hash table if Status == First, previous entry in the correspondence table if Status == Subsequent
+    public int Previous; // index in the hash table if Status == First, previous entry in the correspondence table if Status == Subsequent
     public EntryStatus Status;
     public int DataIndex; // Index in the data table
     public int Next; // Index of next entry in the correspondence table, -1 if this is last
+
+    public string Debug()
+    {
+        return Status switch
+        {
+            EntryStatus.Unused => "\u2205",
+            EntryStatus.First => Next == NoNextCorrespondence ?
+                $"{Previous} \u2190 (\u00B7 \u00B7, \u00B7 \u00B7) \u2192 {DataIndex}" :
+                $"{Previous} \u2190 (\u00B7 \u00B7, \u2193 {Next}) \u2192 {DataIndex}",
+            EntryStatus.Subsequent => Next == NoNextCorrespondence ?
+                $"\u00B7 \u2190 (\u2191 {Previous}, \u00B7 \u00B7) \u2192 {DataIndex}" :
+                $"\u00B7 \u2190 (\u2191 {Previous}, \u2193 {Next}) \u2192 {DataIndex}",
+            _ => throw new InvalidDataException(),
+        };
+    }
 }
--- NaryCollections/Details/HashEntry.cs
+++ NaryCollections/Details/HashEntry.cs
@@ -1,9 +1,23 @@
+using System.Diagnostics;
+
 namespace NaryCollections.Details;
 
+[DebuggerDisplay("{this.Debug()}")]
 public struct HashEntry
 {
     public static readonly uint DriftForUnused = 0;
     
     public int ForwardIndex; // First appropriate index in the correspondence table
     public uint DriftPlusOne; // 0 if unused, if > 0 represent a drift of (DriftPlusOne - 1)
+
+    public string Debug()
+    {
+        return DriftPlusOne == DriftForUnused ? "\u2205" : $"{ToText(DriftPlusOne)} \u2192 {ForwardIndex}";
+    }
+
+    private string ToText(uint driftPlusOne)
+    {
+        if (driftPlusOne == 1) return "\u2713";
+        return new string('\u25bc', (int)driftPlusOne - 1);
+    }
 }
