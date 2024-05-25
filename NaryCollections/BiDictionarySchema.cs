0ed4b449c0831b6965e3a17b8439771806b1f387
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2024-05-26 01:00:06 +0200

    BiDictionarySchema

---
+++ NaryCollections/BiDictionarySchema.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics.Contracts;
+
+namespace NaryCollections;
+
+public sealed class BiDictionarySchema<TDirect, TInverse> : Schema<(TDirect, TInverse)>
+{
+    public SearchableParticipant<TInverse> Inverse { get;  }
+
+    public SearchableParticipant<TDirect> Direct { get; }
+    
+    public BiDictionarySchema()
+    {
+        Direct = AddSearchableParticipant<TDirect>(unique: true);
+        Inverse = AddSearchableParticipant<TInverse>(unique: true);
+        Sign = Conclude(Direct, Inverse);
+    }
+
+    protected override Signature Sign { get; }
+}
+
+public sealed class BiMultiDictionarySchema<TDirect, TInverse> : Schema<(TDirect, TInverse)>
+{
+    public SearchableParticipant<TInverse> Inverse { get;  }
+
+    public SearchableParticipant<TDirect> Direct { get; }
+    
+    public BiMultiDictionarySchema()
+    {
+        Direct = AddSearchableParticipant<TDirect>(unique: false);
+        Inverse = AddSearchableParticipant<TInverse>(unique: false);
+        Sign = Conclude(Direct, Inverse);
+    }
+
+    protected override Signature Sign { get; }
+}
