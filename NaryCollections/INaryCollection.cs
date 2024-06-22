812df702e2b07fe26bc22ca1aba21dc4146704f6
Eddy Cizeron <eddy.cizeron@shift-technology.com>
2024-06-22 13:00:44 +0200

    NaryCollection as Set

--- NaryCollections/INaryCollection.cs
+++ NaryCollections/INaryCollection.cs
@@ -3,7 +3,14 @@ namespace NaryCollections;
 public interface IReadOnlyNaryCollection<out TSchema> where TSchema : Schema
 {
     public TSchema Schema { get; }
-    public IReadOnlyCollection<object?> GetUntypedSet();
+    
+    public abstract IRelationSelection<TSchema, T> With<T>(Func<TSchema, SearchableComposite<T>> selector);
+
+    public abstract IRelationSelection<TSchema, T> With<T>(Func<TSchema, SearchableParticipant<T>> selector);
+
+    public abstract IOrderedRelationSelection<TSchema, T> With<T>(Func<TSchema, OrderedComposite<T>> selector);
+
+    public abstract IOrderedRelationSelection<TSchema, T> With<T>(Func<TSchema, OrderedParticipant<T>> selector);
 }
 
 public interface INaryCollection<out TSchema> : IReadOnlyNaryCollection<TSchema> where TSchema : Schema
--- NaryCollections/NaryCollection.cs
+++ NaryCollections/NaryCollection.cs
@@ -3,28 +3,24 @@ using System.Runtime.CompilerServices;
 
 namespace NaryCollections;
 
-public abstract class NaryCollection<TSchema> : INaryCollection<TSchema> where TSchema : Schema, new()
-{
-    public TSchema Schema => throw new NotImplementedException();
-    
-    public abstract IReadOnlyCollection<object?> GetUntypedSet();
-
-    public static NaryCollection<TSchema> New() => throw new NotImplementedException();
-
-    public abstract IRelationSelection<TSchema, T> With<T>(Func<TSchema, SearchableComposite<T>> selector);
-
-    public abstract IRelationSelection<TSchema, T> With<T>(Func<TSchema, SearchableParticipant<T>> selector);
-
-    public abstract IOrderedRelationSelection<TSchema, T> With<T>(Func<TSchema, OrderedComposite<T>> selector);
-
-    public abstract IOrderedRelationSelection<TSchema, T> With<T>(Func<TSchema, OrderedParticipant<T>> selector);
-}
-
 public static class NaryCollection
 {
-    public static IReadOnlySet<TArgTuple> AsSet<TArgTuple>(this IReadOnlyNaryCollection<Schema<TArgTuple>> collection)
+    public static INaryCollection<TSchema> New<TSchema>() where TSchema : Schema, new()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static ISet<TArgTuple> AsSet<TArgTuple>(
+        this INaryCollection<Schema<TArgTuple>> collection)
         where TArgTuple : struct, ITuple, IStructuralEquatable
     {
-        return (IReadOnlySet<TArgTuple>)collection.GetUntypedSet();
+        return (ISet<TArgTuple>)collection;
+    }
+    
+    public static IReadOnlySet<TArgTuple> AsReadOnlySet<TArgTuple>(
+        this IReadOnlyNaryCollection<Schema<TArgTuple>> collection)
+        where TArgTuple : struct, ITuple, IStructuralEquatable
+    {
+        return (IReadOnlySet<TArgTuple>)collection;
     }
 }
